d871f391d166f87f42b259ab63961ea40169cbe7
Evan Vilagov <evan.vilagov@gmail.com>
2021-06-09 15:48:03 +0300

    Updated scheduler.py where PATH scpecified before each command

--- scheduler.py
+++ scheduler.py
@@ -7,22 +7,21 @@ USER = getpass.getuser()
 
 
 cron = CronTab(user=USER)
-cron.env['HOME'] = os.getcwd()
-cron.env['PATH'] = os.getcwd()
+PATH = os.getcwd()
 
-notify = cron.new(command='.venv/bin/python notifier.py -c kpi-first')
+notify = cron.new(command=f'{PATH}/.venv/bin/python notifier.py -c kpi-first')
 notify.setall('0 20 * * 1-5')
 
-notify = cron.new(command='.venv/bin/python notifier.py -c kpi-second')
+notify = cron.new(command=f'{PATH}/.venv/bin/python notifier.py -c kpi-second')
 notify.setall('30 20 * * 1-5')
 
-update = cron.new(command='.venv/bin/python updater.py -c day')
+update = cron.new(command=f'{PATH}/.venv/bin/python updater.py -c day')
 update.setall('0 21 * * 1-5')
 
-update = cron.new(command='.venv/bin/python updater.py -c week')
+update = cron.new(command=f'{PATH}/.venv/bin/python updater.py -c week')
 update.setall('5 21 * * 5')
 
-update = cron.new(command='.venv/bin/python updater.py -c lawsuits')
+update = cron.new(command=f'{PATH}/.venv/bin/python updater.py -c lawsuits')
 update.setall('0 18 * * 5')
 
 cron.write()
