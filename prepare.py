36ac9c677eb545f809144118a0853e782bd4490d
Evan Vesmirov <evan.vesmirov@proton.me>
2022-11-01 02:37:45 +0400

    Fix critical bug with database creation

--- prepare.py
+++ prepare.py
@@ -19,7 +19,7 @@ def main():
     try:
         cursor.execute(
             'CREATE TABLE employees ('
-            'user_id INT NOT NULL UNIQUE,'
+            'user_id BIGINT NOT NULL UNIQUE,'
             'username VARCHAR(100) NOT NULL,'
             'firstname VARCHAR(100) NOT NULL,'
             'lastname VARCHAR(100),'
